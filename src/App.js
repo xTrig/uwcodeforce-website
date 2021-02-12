a3decb49b0321e4f3ebf986b112d007dbbe3aab5
Steven <steven.simko1000@gmail.com>
2021-02-12 09:44:02 -0600

    Started on login states

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Head from './components/Head/Head';
 import Contact from './components/Contact/Contact';
 import Challenges from './components/Challenges/Challenges';
 import Footer from './components/Footer/Footer.component';
+import {useState} from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 // import ppt from './codeforceppt.pdf';
 
 
 const App = () => {
+
+  const [signedIn, setSignedIn] = useState();
   return (
     <div className="fullscreen">
       <Router>
-        <Head/>
+        <Head signedIn={signedIn} setSignedIn={setSignedIn}/>
         <Switch>
           <Route path="/challenges"><Challenges/></Route>
           <Route path="/contact"><Contact/></Route>
