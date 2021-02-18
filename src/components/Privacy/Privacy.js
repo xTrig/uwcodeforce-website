182e9c5aa754501ffffe9406a12dc067a33e0365
Steven <steven.simko1000@gmail.com>
2021-02-18 11:41:09 -0600

    Added quick privacy policy

--- src/components/Privacy/Privacy.js
+++ src/components/Privacy/Privacy.js
@@ -13,6 +13,8 @@ const Privacy = () => {
                 <p><i>Your Google Id and Email:</i> For authentication purposes.</p>
                 <p><i>Your First and Last Name:</i> We collect your first and last name from your Google account to speed up the registration process. This data can be changed when you sign up.</p>
                 <p><i>Your contact email:</i> An email address different from your Google account email, if you prefer a different address to be contacted from.</p>
+                <p className="mt-3"><b>Who we share this information with</b></p>
+                <p>We do not share this information with anybody outside of the UW Code Force team.</p>
             </div>
         </div>
     )
