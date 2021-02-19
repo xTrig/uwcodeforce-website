3ae7f8e8d4b1c996ea3d99f77098293649dd7274
Steven <steven.simko1000@gmail.com>
2021-02-19 10:03:13 -0600

    Changed link names to make them less confusing

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -29,8 +29,8 @@ const Head = ({signedIn, setSignedIn}) => {
             <Link className="nav-link dark-hover text-light" to="/challenges">Challenges</Link>
             <Link className="nav-link dark-hover text-light" to="/contact"> Contact </Link>
             {signedIn ? 
-            <Link className="nav-link dark-hover text-light" to="/signup">Sign Up</Link>
-            : <Link className="nav-link dark-hover disabled" to="/signup">Sign Up</Link>
+            <Link className="nav-link dark-hover text-light" to="/signup">Challenge Sign Up</Link>
+            : <Link className="nav-link dark-hover disabled" to="/signup">Challenge Sign Up</Link>
             }
             
           </div>
@@ -38,7 +38,7 @@ const Head = ({signedIn, setSignedIn}) => {
         </div>
         <div className="nav ml-auto">
             {!signedIn ? 
-            <a className="nav-link dark-hover text-light" href="https://uwcodeforce.ca/api/auth" rel="noreferrer">Login</a>
+            <a className="nav-link dark-hover text-light" href="https://uwcodeforce.ca/api/auth" rel="noreferrer">Login/Register</a>
             : <a className="nav-link dark-hover text-light" href="https://uwcodeforce.ca/api/auth/logout" rel="noreferrer">Logout</a>}
           </div>
       </div>
